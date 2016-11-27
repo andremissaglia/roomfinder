ccb8591c54226cb1bb5ab7235fc9fd61bbb9a3c6
Gustavo Ceccon <gustavofceccon@gmail.com>
2016-11-27 20:28:17 -0200

    Buttons

--- www/js/FreeCanvas.js
+++ www/js/FreeCanvas.js
@@ -4,15 +4,32 @@ angular.module('roomfinder.services')
     setCanvas: function (canvasID, onDraw, afterDraw){
       var canvas = document.getElementById(canvasID);
       var context = canvas.getContext("2d");
+      var buttons = [];
       var resizeCanvas = function () {
         canvas.height = window.innerHeight;
         canvas.width = window.innerWidth;
+        draw();
       };
       window.addEventListener('resize', resizeCanvas, false);
-      resizeCanvas();
 
       var transform = math.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
-
+      var addButton = function(button) {
+        button.id = Math.floor((1 + Math.random()) * 0x10000);
+        buttons.push(button);
+        return button.id;
+      };
+      var removeButton = function(button) {
+        var index = buttons.indexOf(button);
+        if(index > -1)
+          buttons.splice(index, 1);
+      };
+      var checkCollision = function(button, x, y) {
+        return (button.min.x < x && x < button.max.x &&
+        button.min.y < y && y < button.max.y);
+      };
+      var getBounds = function () {
+        return { width: canvas.width, height: canvas.height };
+      };
       var draw = function(){
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.save();
@@ -28,6 +45,11 @@ angular.module('roomfinder.services')
         onDraw(context);
         context.restore();
         afterDraw(context);
+
+        buttons.forEach(function (button) {
+          if(button.img != null && button.img != undefined)
+            context.drawImage(button.img, button.min.x, button.min.y, button.width, button.height);
+        });
       };
       var move = function(x,y){
         var translate = math.matrix([[1, 0, x], [0, 1, y], [0, 0, 1]]);
@@ -49,12 +71,18 @@ angular.module('roomfinder.services')
         var posCanvas = math.multiply(transform, [posWorld[0], posWorld[1], 1]);
         return [
           posCanvas.subset(math.index(0)),
-          posCanvas.subset(math.index(1)),
+          posCanvas.subset(math.index(1))
         ]
-      }
+      };
 
       // drag
       var isDragging = false;
+      canvas.addEventListener('click', function(event){
+        buttons.forEach(function (button) {
+          if(checkCollision(button, event.x, event.y))
+            button.onClick();
+        });
+      });
       canvas.addEventListener('mousedown', function(event){
         isDragging = true;
       });
@@ -146,13 +174,18 @@ angular.module('roomfinder.services')
       });
       mc.on('pinchend', function (event){
         lastScale = 1;
-      })
+      });
+
+      resizeCanvas();
 
       return {
         draw: draw,
         move: move,
         zoom: zoom,
-        world2canvas: world2canvas
+        world2canvas: world2canvas,
+        addButton: addButton,
+        removeButton: removeButton,
+        getBounds: getBounds
       }
     }
   };
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,16 +4,125 @@ angular.module('roomfinder.controllers', [])
   var mapa = Mapas.mapas[0];
   var gps = Rooms.get(0).pos;
   var posOptions = {timeout: 10000, enableHighAccuracy: true};
+  var magOffset = {
+    x: 100,
+    y: 20,
+    padding: 10
+  };
+  var floorOffset = {
+    x: 20,
+    y: 100,
+    padding: 10
+  };
+  var magnify = {
+    min: {
+      x: 0,
+      y: 0
+    },
+    max: {
+      x: 0,
+      y: 0
+    },
+    width: 64,
+    height: 64,
+    onClick: function () {
+      console.log('Magnify');
+    }
+  };
+  magnify.img = new Image();
+  magnify.img.src = 'img/magnify.png';
+  var reduce = {
+    min: {
+      x: 0,
+      y: 0
+    },
+    max: {
+      x: 0,
+      y: 0
+    },
+    width: 64,
+    height: 64,
+    onClick: function () {
+      console.log('Reduce');
+    }
+  };
+  reduce.img = new Image();
+  reduce.img.src = 'img/reduce.png';
+  var upstairs = {
+    min: {
+      x: 0,
+      y: 0
+    },
+    max: {
+      x: 0,
+      y: 0
+    },
+    width: 64,
+    height: 64,
+    onClick: function () {
+      console.log('Up');
+    }
+  };
+  upstairs.img = new Image();
+  upstairs.img.src = 'img/up.png';
+  var downstairs = {
+    min: {
+      x: 0,
+      y: 0
+    },
+    max: {
+      x: 0,
+      y: 0
+    },
+    width: 64,
+    height: 64,
+    onClick: function () {
+      console.log('Down');
+    }
+  };
+  downstairs.img = new Image();
+  downstairs.img.src = 'img/down.png';
   var canvas =  FreeCanvas.setCanvas('canvas', function (context) {
+    if(canvas == undefined)
+      return;
     context.drawImage(sticky, 0, 0);
 
   }, function (context) {
+    if(canvas == undefined)
+      return;
     if(gps[0] != 0 && gps[1] != 0){
       var pos = Mapas.converter(mapa.transform, mapa.p0, gps);
       pos = canvas.world2canvas(pos);
       context.drawImage(point, pos[0]-14, pos[1]-40);
+
     }
+
+    var bounds = canvas.getBounds();
+    reduce.min.x = bounds.width - magnify.width - magOffset.x;
+    reduce.min.y = bounds.height - magnify.height - magOffset.y;
+    magnify.min.x = bounds.width - magnify.width - reduce.width - magOffset.padding - magOffset.x;
+    magnify.min.y = bounds.height - magnify.height - magOffset.y;
+
+    reduce.max.x = reduce.min.x + reduce.width;
+    reduce.max.y = reduce.min.y + reduce.height;
+    magnify.max.x = magnify.min.x + magnify.width;
+    magnify.max.y = magnify.min.y + magnify.height;
+
+    downstairs.min.x = bounds.width - downstairs.width - floorOffset.x;
+    downstairs.min.y = bounds.height - downstairs.height - floorOffset.y;
+    upstairs.min.x = bounds.width - upstairs.width - floorOffset.x;
+    upstairs.min.y = bounds.height - upstairs.height - downstairs.height - floorOffset.padding - floorOffset.y;
+
+    downstairs.max.x = downstairs.min.x + downstairs.width;
+    downstairs.max.y = downstairs.min.y + downstairs.height;
+    upstairs.max.x = upstairs.min.x + upstairs.width;
+    upstairs.max.y = upstairs.min.y + upstairs.height;
   });
+
+  canvas.addButton(magnify);
+  canvas.addButton(reduce);
+  canvas.addButton(upstairs);
+  canvas.addButton(downstairs);
 
   var sticky = new Image();
   sticky.src = mapa.src;
